98f507061562bbe2042eb618a31a5dd42517befa
Thomas Boer <203590@novacollege.nl>
2026-03-23 09:01:33 +0100

    [ADD] main layout assignment8

--- student_assignments/assignment8.php
+++ student_assignments/assignment8.php
@@ -1,10 +1,17 @@
 <?php
-    session_start();
     require_once __DIR__ . '/../includes/load_data.php';
     $characterDataset = load_data();
 
+
+    session_start();
+    while ($_SESSION['secret_character'] == "_feature_order" or $_SESSION['secret_character'] == "")
+    {
+        $_SESSION['secret_character'] = array_rand($characterDataset);
+    }
+
+
     // Opdracht 7: Bouw het "Wie is het?" spel.
-    // 1. Kies een willekeurig personage en sla dit op in de sessie.
+    // 1. Kies een willekeurig personage en sla dit op in de sessie.//
     // 2. Maak een formulier waarmee de speler een feature kan kiezen om te vragen.
     // 3. Vergelijk de gekozen feature met die van het geheime personage.
     // 4. Geef antwoord ("Ja" of "Nee").
@@ -17,9 +24,48 @@
 <head>
     <meta charset="utf-8">
     <title>Guess Who – Board Game</title>
-    <!-- je mag met AI een passende CSS thema genereren -->
+    <link rel="stylesheet" href="../css/assignment8.css">
 </head>
 <body>
-    <!-- Hier komt de HTML voor je spel. Bouw de interface met de game board, de vragen en de kandidaten. -->
+
+    <div id="main_container">
+        <div id="form_container">
+            <div id="form_title">
+                <label for="#form"><b>Guess Who</b></label>
+            </div>
+            <div id="questions">
+                <form method="post" id="form">
+                    <label for="form_radio" id="title"><b>Does Your Character have<br>/ Is Your Character</b></label><br>
+
+                    <div id="scroll">
+                    <input class="radio" name="attributes" type="radio" id="man"> <label for="man"><b>A Man?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="woman"> <label for="woman"><b>A Woman?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="blond"> <label for="blond"><b>Blond Hair?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="brown"> <label for="brown"><b>Brown Hair?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="black"> <label for="black"><b>Black Hair?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="red"> <label for="red"><b>Red Hair?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="white"> <label for="white"><b>White Hair?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="bald"> <label for="bald"><b>Bald?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="mustache"> <label for="mustache"><b>A Mustache?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="beard"> <label for="beard"><b>A Beard?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="glasses"> <label for="glasses"><b>Glasses?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="hat"> <label for="hat"><b>A Hat?</b></label><br>
+                    <input class="radio" name="attributes" type="radio" id="earings"> <label for="earings"><b>Earings?</b></label><br>
+                    </div>
+
+                    <input type="submit" value="Guess">
+                </form>
+            </div>
+            <div id="form_answer">
+                <b>Yes/No</b>
+            </div>
+
+
+        </div>
+        <div id="people_container">
+
+        </div>
+    </div>
+
 </body>
 </html>
